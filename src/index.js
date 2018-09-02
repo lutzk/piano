b8bdbc309505cbbe18dc3872cc6ccfb89d3e8c2e
jo <lutz.kwauka@gmail.com>
2018-09-02 21:06:57 +0200

    app component - use components

--- src/index.js
+++ src/index.js
@@ -1,116 +1,185 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
+import { find } from 'lodash';
 import 'react-piano/build/styles.css';
 
-import DimensionsProvider from './DimensionsProvider';
-import SoundfontProvider from './SoundfontProvider';
+import ControlPanel from './ControlPanel';
+import SongsList from './SongsList';
+import MyPiano from './Piano';
 import './styles.css';
 
-// webkitAudioContext fallback needed to support Safari
-const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-const soundfontHostname = 'https://d1pzp51pvbm36p.cloudfront.net';
+class App extends React.Component {
 
-const noteRange = {
-  first: MidiNumbers.fromNote('c3'),
-  last: MidiNumbers.fromNote('f4'),
-};
-const keyboardShortcuts = KeyboardShortcuts.create({
-  firstNote: noteRange.first,
-  lastNote: noteRange.last,
-  keyboardConfig: KeyboardShortcuts.HOME_ROW,
-});
+  constructor(props) {
+    super(props);
+    this.state = {
+      song: [],
+      songsList: [],
+      isPlaying: false,
+      isRecording: false,
+      selectedSong: null,
+      showNameForm: false,
+      showSaveDialog: false,
+      recordStartTime: null,
+    };
+    this.input = React.createRef();
+  }
 
-function App() {
-  return (
-    <div>
-      <h1>react-piano demos</h1>
-      <div className="mt-5">
-        <p>Basic piano with hardcoded width</p>
-        <BasicPiano />
-      </div>
+  componentDidMount = () => {
+    this.setState({
+      songsList: this.loadSongsFromStorage(),
+    });
+  }
 
-      <div className="mt-5">
-        <p>
-          Responsive piano which resizes to container's width. Try resizing the
-          window!
-        </p>
-        <ResponsivePiano />
-      </div>
+  playSong = () => {
+    this.setState({
+      isPlaying: true,
+    });
+  }
 
-      <div className="mt-5">
-        <p>Piano with custom styling - see styles.css</p>
-        <ResponsivePiano className="PianoDarkTheme" />
-      </div>
-    </div>
-  );
-}
+  addNoteToSong = ({ id, midi, startTime }) => {
+    // console.log('ADD:', ({ id, midi, startTime }));
+    this.state.song.push({ id, midi, startTime });
+    // console.log('ADD:', this.state.song);
+  }
 
-function BasicPiano() {
-  return (
-    <SoundfontProvider
-      instrumentName="acoustic_grand_piano"
-      audioContext={audioContext}
-      hostname={soundfontHostname}
-      render={({ isLoading, playNote, stopNote }) => (
-        <Piano
-          noteRange={noteRange}
-          width={300}
-          onPlayNote={playNote}
-          onStopNote={stopNote}
-          disabled={isLoading}
-          keyboardShortcuts={keyboardShortcuts}
+  startRecording = () => {
+    this.setState({
+      isRecording: true,
+      recordStartTime: Date.now(),
+    });
+  }
+
+  stopRecording = () => {
+    this.setState({
+      isRecording: !this.state.isRecording,
+      showSaveDialog: true,
+    });
+  }
+
+  discartRecord = () => {
+    this.setState({
+      song: [],
+      showSaveDialog: false
+    });
+  }
+
+  saveRecord = () => {
+    this.setState({
+      showNameForm: true,
+    });
+  }
+
+  updateSong = song => {
+    this.setState({
+      song
+    });
+  }
+
+  loadSongsFromStorage = () => {
+    let songs = [];
+    try {
+      songs = JSON.parse(localStorage.getItem('songs'));
+    } catch (e) {
+    }
+    return songs;
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const newSongsList = [
+      { name: this.input.current.value, value: this.state.song },
+      ...this.state.songsList,
+    ];
+    localStorage.setItem(
+      'songs',
+      JSON.stringify(newSongsList)
+    )
+    this.setState({
+      showSaveDialog: false,
+      showNameForm: false,
+      songsList: newSongsList,
+    });
+    this.input.current.value = '';
+  }
+
+  resetPlay = () => {
+    this.setState({
+      isPlaying: false,
+    })
+  }
+
+  selectSong = name => {
+    this.setState({
+      selectedSong: find(this.state.songsList, { name }),
+    });
+  }
+
+  render() {
+    const {
+      song,
+      isPlaying,
+      songsList,
+      isRecording,
+      selectedSong,
+      showNameForm,
+      showSaveDialog,
+      recordStartTime,
+    } = this.state;
+
+    const {
+      playSong,
+      resetPlay,
+      saveRecord,
+      updateSong,
+      selectSong,
+      discartRecord,
+      stopRecording,
+      addNoteToSong,
+      startRecording,
+    } = this;
+
+    return (
+      <div>
+        <h1>react-piano demos</h1>
+        <ControlPanel
+          playSong={playSong}
+          isPlaying={isPlaying}
+          saveRecord={saveRecord}
+          isRecording={isRecording}
+          selectedSong={selectedSong}
+          discartRecord={discartRecord}
+          stopRecording={stopRecording}
+          showSaveDialog={showSaveDialog}
+          startRecording={startRecording}
         />
-      )}
-    />
-  );
-}
 
-function ResponsivePiano(props) {
-  return (
-    <DimensionsProvider>
-      {({ containerWidth, containerHeight }) => (
-        <SoundfontProvider
-          instrumentName="acoustic_grand_piano"
-          audioContext={audioContext}
-          hostname={soundfontHostname}
-          render={({ isLoading, playNote, stopNote }) => (
-            <Piano
-              noteRange={noteRange}
-              width={containerWidth}
-              onPlayNote={playNote}
-              onStopNote={stopNote}
-              disabled={isLoading}
-            />
-          )}
-        />
-      )}
-    </DimensionsProvider>
-  );
-}
+        {showNameForm &&
+          <div>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="song name" name="songName" ref={this.input} />
+              <button>save</button>
+            </form>
+          </div>}
 
-function ResponsivePiano(props) {
-  return (
-    <DimensionsProvider>
-      {({ containerWidth, containerHeight }) => (
-        <SoundfontProvider
-          instrumentName="acoustic_grand_piano"
-          audioContext={audioContext}
-          hostname={soundfontHostname}
-          render={({ isLoading, playNote, stopNote }) => (
-            <Piano
-              noteRange={noteRange}
-              width={containerWidth}
-              onPlayNote={playNote}
-              onStopNote={stopNote}
-              disabled={isLoading}
-              {...props}
-            />
-          )}
-        />
-      )}
-    </DimensionsProvider>
-  );
+        <div>
+          <MyPiano
+            song={song}
+            isPlaying={isPlaying}
+            resetPlay={resetPlay}
+            updateSong={updateSong}
+            isRecording={isRecording}
+            selectedSong={selectedSong}
+            addNoteToSong={addNoteToSong}
+            showSaveDialog={showSaveDialog}
+            recordStartTime={recordStartTime}
+          />
+        </div>
+
+        <SongsList songsList={songsList} selectSong={selectSong} />
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById('root');
